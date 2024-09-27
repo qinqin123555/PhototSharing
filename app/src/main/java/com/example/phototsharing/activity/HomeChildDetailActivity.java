38f544710e339fcf48cce8fc9a45f1dbffdd7d4f
DM85 <DM3177504166@OUTLOOK.com>
2024-09-27 20:36:32 +0800

    首页子项详情成功加载图片

--- app/src/main/java/com/example/phototsharing/activity/HomeChildDetailActivity.java
+++ app/src/main/java/com/example/phototsharing/activity/HomeChildDetailActivity.java
@@ -1,8 +1,15 @@
 package com.example.phototsharing.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -10,7 +17,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.phototsharing.R;
 import com.example.phototsharing.adapter.HomeChildDetailImageAdapter;
@@ -26,13 +34,26 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeChildDetailActivity extends AppCompatActivity {
     private ShareDetailBean myShareDetailBean;
-    private ApiInterface myApiInterface;
-    private Retrofit myRetrofit;
     private HomeChildDetailImageAdapter homeChildDetailImageAdapter;
-    private ViewPager2 imageViewPager;
+    private Context myContext;
+
+    private ImageView backImg;
+    private ImageView profilePicture;
+    private TextView userName;
+    private Button focusBtn;
+
+    private ScrollView homeChildDetailScrollView;
+    private RecyclerView homeChildDetailImageRecyclerView;
+    private TextView homeChildDetailTitle;
+    private TextView homeChildDetailContent;
+    private EditText homeChildDetailCommentEditText;
+    private ImageView icLike;
+    private TextView likeNum;
+    private ImageView icCollect;
+    private TextView collectNum;
 
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,57 +66,48 @@ public class HomeChildDetailActivity extends AppCompatActivity {
         });
 
 
-
-
 //        接收HomeChildFragment传来的shareId
         Intent intent = getIntent();
         long shareId = intent.getLongExtra("shareId",0);
 
-        imageViewPager = findViewById(R.id.home_child_detail_image_viewpager);
+        //初始化一些参数
+        myContext = getApplicationContext();
         myShareDetailBean = new ShareDetailBean();
+        homeChildDetailImageAdapter = new HomeChildDetailImageAdapter();
+        homeChildDetailScrollView = findViewById(R.id.home_child_detail_scroll_view);
+        homeChildDetailImageRecyclerView = findViewById(R.id.home_child_detail_image_recycler_view);
+
+
+
+        //网络请求数据，并渲染UI
         loadData(shareId);
-
-
-
 
     }
 
 
     private void loadData(long shareId) {
-        myRetrofit = new Retrofit.Builder()
+        Retrofit myRetrofit = new Retrofit.Builder()
                 .baseUrl("https://api-store.openguet.cn/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        myApiInterface = myRetrofit.create(ApiInterface.class);
-        Call<ShareDetailBean> call = myApiInterface.getFindDetailInfo(MyHeaders.getAppId(),MyHeaders.getAppSecret(),shareId,0);
+        ApiInterface myApiInterface = myRetrofit.create(ApiInterface.class);
+        Call<ShareDetailBean> call = myApiInterface.getFindDetailInfo(MyHeaders.getAppId(),MyHeaders.getAppSecret(),shareId, 1838948060948992000L);
         call.enqueue(new Callback<ShareDetailBean>() {
             @Override
             public void onResponse(@NonNull Call<ShareDetailBean> call, @NonNull Response<ShareDetailBean> response) {
                 if (response.isSuccessful()) {
                     myShareDetailBean = response.body();
-                    if (myShareDetailBean != null && myShareDetailBean.getData() != null){
-                        homeChildDetailImageAdapter = new HomeChildDetailImageAdapter(getApplicationContext(),myShareDetailBean.getData().getImageUrlList());
-                        imageViewPager.setAdapter(homeChildDetailImageAdapter);
 
-                        imageViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-                            @Override
-                            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-                            }
+                    if (myShareDetailBean != null){
+                        Log.e("TAG", String.valueOf(response.body()));
 
-                            @Override
-                            public void onPageScrollStateChanged(int state) {
-                                super.onPageScrollStateChanged(state);
-                            }
+                        Log.e("TAG", String.valueOf(shareId));
 
-                            @Override
-                            public void onPageSelected(int position) {
-                                super.onPageSelected(position);
-                            }
-                        });
+                        initView(myShareDetailBean);
+
                     }
                 } else{
-                    Log.e("TAG","接收数据失败");
+
                 }
             }
 
@@ -105,6 +117,55 @@ public class HomeChildDetailActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    @SuppressLint({"SetTextI18n", "DefaultLocale"})
+    private void initView(ShareDetailBean shareDetailBean){
+        backImg = findViewById(R.id.iv_back);
+        profilePicture = findViewById(R.id.profile_picture);
+        userName = findViewById(R.id.username);
+        focusBtn = findViewById(R.id.focus);
+        homeChildDetailTitle = findViewById(R.id.tv_home_child_detail_title);
+        homeChildDetailContent = findViewById(R.id.tv_home_child_detail_content);
+        homeChildDetailCommentEditText = findViewById(R.id.home_child_detail_comment_edit_text);
+        icLike = findViewById(R.id.iv_home_child_detail_like);
+        likeNum = findViewById(R.id.tv_home_child_detail_like_number);
+        icCollect = findViewById(R.id.iv_home_child_detail_collect);
+        collectNum = findViewById(R.id.tv_home_child_detail_collect_number);
+
+
+        homeChildDetailImageAdapter.changeAdapter(myContext,shareDetailBean.getData().getImageUrlList());
+        homeChildDetailImageRecyclerView.setLayoutManager(new LinearLayoutManager(myContext,LinearLayoutManager.HORIZONTAL,false));
+        homeChildDetailImageRecyclerView.setAdapter(homeChildDetailImageAdapter);
+
+        if (shareDetailBean.getData().getUsername() != null) {
+            userName.setText(shareDetailBean.getData().getUsername());
+        } else {
+            userName.setText("username");
+        }
+
+        if (shareDetailBean.getData().getTitle() != null) {
+            homeChildDetailTitle.setText(shareDetailBean.getData().getTitle());
+        } else {
+            homeChildDetailTitle.setText("标题");
+        }
+        if (shareDetailBean.getData().getContent() != null) {
+            homeChildDetailContent.setText(shareDetailBean.getData().getContent());
+        } else {
+            homeChildDetailContent.setText("内容");
+        }
+        if (shareDetailBean.getData().getLikeNum() != null) {
+            likeNum.setText(String.format("%d",shareDetailBean.getData().getLikeNum()));
+        } else {
+            likeNum.setText("0");
+        }
+
+        if (shareDetailBean.getData().getCollectNum() != null) {
+            collectNum.setText(String.format("%d",shareDetailBean.getData().getCollectNum()));
+        } else {
+            collectNum.setText("0");
+        }
 
 
     }
